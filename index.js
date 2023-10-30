7855549d4dfee864706ff331aff0939c6f1f778e
Derek Mercedes <mercedes.derek@gmail.com>
2023-10-29 18:16:09 -0600

    does literally nothing but is syntatically correct, I guess?

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+   callback()
+}
+
+function returnsANamedFunction(){
+
+    return function returnedFunction(){
+
+    }
+
+
+}
+
+function returnsAnAnonymousFunction(){
+    return ()=>{}
+}
